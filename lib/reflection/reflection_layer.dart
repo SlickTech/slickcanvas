5559c3ecb5377454dcc1a82c7e551c27899fd6b2
kzhdev <kun.zhao@tradingtechnologies.com>
2014-10-12 19:54:57 -0500

    Fix reflection didn't maintain correct layering order

--- lib/reflection/reflection_layer.dart
+++ lib/reflection/reflection_layer.dart
@@ -2,82 +2,84 @@ part of smartcanvas;
 
 class _ReflectionLayer extends Layer implements I_Container_Reflection {
 
-  Node _node;
-  _ReflectionLayer _layer;
-  SvgLayer _impl;
+    Node _node;
+    _ReflectionLayer _layer;
+    SvgLayer _impl;
+    Map<Layer, num> _layerReflectionMap = {};
 
-  _ReflectionLayer(Map<String, dynamic> config)
-    :super(svg, merge(config, {
-      ID: '__reflection_layer',
-      OPACITY: 0
-    }))
-  {}
+    _ReflectionLayer(Map<String, dynamic> config)
+            : super(svg, merge(config, {
+                ID: '__reflection_layer',
+                OPACITY: 0
+            }));
+    void addChild(Node child) {
+        if (!(child is I_Reflection)) {
+            throw 'Reflection Layer can only add reflection node';
+        }
 
-  void addChild(Node child) {
-    if (!(child is I_Reflection)) {
-      throw 'Reflection Layer can only add reflection node';
+        super.addChild(child);
     }
 
-    super.addChild(child);
-  }
+    void insertChild(int index, Node node) {
+        if (!(node is I_Reflection)) {
+            throw 'Reflection Layer can only add reflection node';
+        }
 
-  void insertChild(int index, Node node) {
-    if (!(node is I_Reflection)) {
-      throw 'Reflection Layer can only add reflection node';
+        super.insertChild(index, node);
     }
 
-    super.insertChild(index, node);
-  }
-
-  void insertNode(I_Reflection node) {
-    // find next reflectable node in the same layer
-    Node realNode = node._node;
-    Node nextReflectableNode = realNode.layer.firstReflectableNode(startIndex:realNode.layer._children.indexOf(realNode) + 1);
-    if (nextReflectableNode != null) {
-      insertChild(_children.indexOf(nextReflectableNode._reflection as Node), node as Node);
-    } else {
-      reflectNode(realNode);
-    }
-  }
-
-  void reflectionAdd(Node child) {
-    reflectNode(child);
-  }
-
-  void reflectNode(Node node) {
-    if (node.layer == null) {
-      return;
+    void insertNode(I_Reflection node) {
+        // find next reflectable node in the same layer
+        Node realNode = node._node;
+        Node nextReflectableNode = realNode.layer.firstReflectableNode(startIndex: realNode.layer._children.indexOf(realNode) + 1);
+        if (nextReflectableNode != null) {
+            insertChild(_children.indexOf(nextReflectableNode._reflection as Node), node as Node);
+        } else {
+            reflectNode(realNode);
+        }
     }
 
-    if (!node.reflectable) {
-      return;
+    void reflectionAdd(Node child) {
+        reflectNode(child);
     }
 
-    var reflection = _createReflection(node);
+    void reflectNode(Node node) {
+        if (node.layer == null) {
+            return;
+        }
 
-    // find top layer
-    var topLayerIndex = _parent._children.length - 1;
+        if (!node.reflectable) {
+            return;
+        }
 
-    // check if the node is on top layer
-    if (topLayerIndex >= 0 && _parent._children.indexOf(node.layer) < topLayerIndex) {
-      // the node isn't on top layer
-      // insert the node before the first node of the top layer
+        var reflection = _createReflection(node);
 
-      // get top layer
-      var topLayer = _parent._children[topLayerIndex];
+        var node_layer = node.layer;
+        var layerIndex = _parent._children.indexOf(node_layer);
 
-      // find the reflection index of the first node in top layer
-      var firstReflectableNode = topLayer.firstReflectableNode(excludeChild: true);
-      var index = firstReflectableNode == null ? -1 : this._children.indexOf(firstReflectableNode._reflection);
+        if (_layerReflectionMap.containsKey(node_layer)) {
+            ++_layerReflectionMap[node_layer];
+            insertChild(_layerReflectionMap[node_layer], reflection as Node);
+        } else {
+            bool nodeAdded = false;
+            for (num i = _parent.children.length - 1; i >= 0; i--) {
+                var layer = _parent.children[i];
+                if (_layerReflectionMap.containsKey(layer)) {
+                    if (i > layerIndex) {
+                        ++_layerReflectionMap[layer];
+                    } else {
+                        num pos = _layerReflectionMap[layer] + 1;
+                        insertChild(pos, reflection as Node);
+                        _layerReflectionMap[node_layer] = pos;
+                        break;
+                    }
+                }
+            }
 
-      if (index != -1) {
-        insertChild(index, reflection as Node);
-      } else {
-        // top layer doesn't have any reflectable node yet, just add the node
-        addChild(reflection as Node);
-      }
-    } else {
-      addChild(reflection as Node);
+            if (_layerReflectionMap.containsKey(node_layer) == false) {
+                insertChild(0, reflection as Node);
+                _layerReflectionMap[node_layer] = 0;
+            }
+        }
     }
-  }
 }
